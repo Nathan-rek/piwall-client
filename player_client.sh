40a0329c234a9eb1ba285e6f8a6d3c356d93bf7d
Nathan-rek <prost.nathan@yahoo.fr>
2026-01-26 12:46:47 +0100

    aj

---
+++ player_client.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+VIDEO=${2:-"/home/pi-nath/Documents/piwall/video/video.mp4"} # 2ème argument = chemin vidéo
+SOCKET="/tmp/mpvsocket"
+CMD=$1  # play / pause / stop
+
+# Supprimer socket existant si mpv déjà lancé
+rm -f $SOCKET
+
+# Lancer mpv si pas déjà lancé
+# Lancer mpv si pas déjà lancé
+pgrep mpv > /dev/null || nohup mpv --fs --no-border --input-ipc-server=$SOCKET "$VIDEO" >/dev/null 2>&1 &
+sleep 1
+
+case $CMD in
+    play)
+        echo '{ "command": ["set_property", "pause", false] }' | socat - UNIX-CONNECT:$SOCKET
+        ;;
+    pause)
+        echo '{ "command": ["set_property", "pause", true] }' | socat - UNIX-CONNECT:$SOCKET
+        ;;
+    stop)
+        pkill mpv
+        ;;
+    *)
+        echo "Commande inconnue : $CMD"
+        ;;
+esac
+
+wait
